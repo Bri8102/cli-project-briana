0a8f1bb2e68489b8169d44918a0d260adc6ac455
Bri8102 <damioniru16@gmail.com>
2020-09-22 21:41:41 -0400

    updated class

--- lib/games.rb
+++ lib/games.rb
@@ -1,24 +1,24 @@
 module UltraGamer
-  class Games
-      attr_accessor :game_name, :price, :console, :type
+    class Games
+        attr_accessor :game_name, :price, :console, :type
 
-      @@all = []
+        @@all = []
 
-      def initialize(game_name, price, console, type)
-          @game_name = game_name
-          @price = price
-          @console = console
-          @type = type
-          @@all << self
-      end
+        def initialize(game_name, price, console, type)
+            @game_name = game_name
+            @price = price
+            @console = console
+            @type = type
+            @@all << self
+        end
 
-      def self.all
-          @@all
-      end
+        def self.all
+            @@all
+        end
 
-      def self.reset
-          self.all.clear
-      end
+        def self.reset
+            self.all.clear
+        end
 
-  end
+    end
 end
