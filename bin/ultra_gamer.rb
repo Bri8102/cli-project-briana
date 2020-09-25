1b07a185dc60eb4aa1337b8f649bd60c08aff0f7
Bri8102 <41807314+Bri8102@users.noreply.github.com>
2020-09-24 22:28:17 -0400

    Update ultra_gamer.rb

--- bin/ultra_gamer.rb
+++ bin/ultra_gamer.rb
@@ -5,7 +5,7 @@ require 'readline'
 require 'pry'
 
 
-require_relative '../lib/deal_category'
+require_relative '../lib/categories'
 require_relative '../lib/scraper'
 require_relative '../lib/games'
 require_relative '../lib/cli'
